074517bd0c13257a8b6d47b0963de7494ac2166b
1798643961 <1798643961@qq.com>
2022-02-11 16:31:13 +0800

    Fix up

--- src/main/java/com/be/utils/BEListener.java
+++ src/main/java/com/be/utils/BEListener.java
@@ -1,58 +1,17 @@
 package com.be.utils;
 
 import com.be.BEPlugin;
-import com.be.fixes.HeadDropFix;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.BlockState;
-import org.bukkit.entity.Entity;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 
 public class BEListener implements Listener {
 
     private static BEListener INSTANCE = new BEListener();
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void playerDeath(PlayerDeathEvent e) {
-        if (!HeadDropFix.dropSkull) {
-            return;
-        }
-        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
-        if (!e.getEntity().getPlayerProfile().hasTextures()) {
-            return;
-        }
-        ItemMeta meta = head.getItemMeta();
-        SkullMeta skullMeta = (SkullMeta) meta;
-        skullMeta.setPlayerProfile(e.getEntity().getPlayerProfile());
-        head.setItemMeta(skullMeta);
-        e.getEntity().getLocation().getWorld().dropItem(e.getEntity().getLocation(), head);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onChunkLoad(ChunkLoadEvent event) {
-        if(HeadDropFix.scanChunk) {
-            for (BlockState state : event.getChunk().getTileEntities()) {
-                HeadDropFix.checkAndRemoveSkull(state);
-            }
-        }
-        if (HeadDropFix.scanItemFrame) {
-            for (Entity entity : event.getChunk().getEntities()) {
-                HeadDropFix.checkSkullInItemFrame(entity);
-            }
-        }
-    }
 
     public static BEListener getInstance() {
         return INSTANCE;
@@ -73,5 +32,13 @@ public class BEListener implements Listener {
             event.getPlayer().teleportAsync(loc, PlayerTeleportEvent.TeleportCause.PLUGIN);
 
         }
+    }
+
+
+    @EventHandler
+    public void onPerLogin(PlayerLoginEvent login) {
+        Player p = login.getPlayer();
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + p.getName() + "已登录");
+        login.allow();
     }
 }
